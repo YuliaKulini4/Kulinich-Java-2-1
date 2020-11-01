be03d160d8dac7cf4fc6fe4b431bf7ef54ed1c93
ylika <kulini7ch@yandex.ru>
2020-11-01 15:38:08 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main (String[] args) {
+        int accountBalance = 2_000_000_000;
+        int transferAmount = 500_000_000;
+        int currentBalance = accountBalance + transferAmount;
+        System.out.println(currentBalance);
+    }
+
+
+}
